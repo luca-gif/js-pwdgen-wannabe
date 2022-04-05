99d2cc144bd498fb6b1c7d53637b5721947a9afd
luca-gif <luca.89m@gmail.com>
2022-04-05 14:26:53 +0200

    Esercizio Password

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digita il tuo nome");
+let cognome = prompt("Digita il tuo cognome");
+let color = prompt("Digita il nome del tuo colore preferito");
+
+document.getElementById("datiUtente").innerHTML = nome + cognome + color + "22";
